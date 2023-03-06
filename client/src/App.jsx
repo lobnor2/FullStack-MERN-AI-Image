da5ea006b3ba77acde17d830b1ca686094a2b8c5
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-03-06 22:31:19 +0530

    added two new components home and createpost

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { logo } from "./assets";
+import { Home, CreatePost } from "./pages/index";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Home />
+      <CreatePost />
     </div>
   );
 }
