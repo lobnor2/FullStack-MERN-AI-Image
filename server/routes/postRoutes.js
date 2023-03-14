1f254926bd1d27f7e42c03751068321277ca45cb
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-03-14 23:17:24 +0530

    create cloudinary account and names and keys

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import * as dotnev from "dotenv";
-import { v2 as cloudnary } from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
 import Post from "../mongodb/models/post.js";
 
@@ -8,8 +8,9 @@ dotnev.config();
 
 const router = express.Router();
 
-router.route("/").get((req, res) => {
-  res.send("hello from post");
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
 export default router;
