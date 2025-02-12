f453f96497a53fbe3dd6a247eb367432b6df8648
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2025-02-12 08:18:36 -0500

    add react router dom and file saver library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Home, CreatePost } from "./pages";
 
 function App() {
-  return <div className="text-3xl">Hello World</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4">
+          <Link to={"/"}>Logo</Link>
+          <Link to={"/create-post"}>Create</Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
