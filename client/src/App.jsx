2a78038f8940aecca226c40337cc42259a9ef0d6
Lobsang Norbu <50455820+lobnor2@users.noreply.github.com>
2025-02-12 09:08:16 -0500

    add shadcn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,26 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Home, CreatePost } from "./pages";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4">
+        <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b-1 shadow-xs">
           <Link to={"/"}>Logo</Link>
-          <Link to={"/create-post"}>Create</Link>
+          <Link to={"/create-post"}>
+            <Button variant={"outline"} className="cursor-pointer">
+              Create
+            </Button>
+          </Link>
         </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create-post" element={<CreatePost />} />
-        </Routes>
+        <main className="sm:p-8 px-4 py-8 w-full min-h-[calc(100vh-73px)]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-post" element={<CreatePost />} />
+          </Routes>
+        </main>
       </BrowserRouter>
     </div>
   );
